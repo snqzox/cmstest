f6973876da7d176bc60305498d3a0aef5cb1e0a1
Miroslav Bereš <miro.beres1@gmail.com>
2016-04-07 03:21:40 +0200

    delete reference, almost working

---
+++ admin/scripts/delete.php
@@ -0,0 +1,23 @@
+<?php
+
+/*include_once $_SERVER['DOCUMENT_ROOT'].'/admin/scripts/connect.php';*/
+
+	$user = 'root';
+  $pass = '';
+ 	$db = 'cms';
+  $host = 'localhost';
+
+   //creating connection do db
+  $resultdb = new mysqli($host, $user, $pass, $db) or die("Unable to connect to database!");
+
+  $id = isset($_GET['id']) ? $_GET['id'] : '';
+
+  $sql = "DELETE FROM refrences WHERE ID = '$id'";
+  
+//add codition based on sent data-id attribute from getData.js file
+  $result = mysqli_query($resultdb,$sql) or die("Unable to DELETE RECORD");
+
+//header('Location: http://localhost/cmstest/admin/index.php#pages/page-reference-list.php'); 
+?>
+
+
