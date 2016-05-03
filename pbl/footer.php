1fa3a78bb0a01c95bd23017438e5cfa943207dd0
Jakub Bielesch <jakub.bielesch@eller.cz>
2016-05-03 15:44:10 +0200

    Front end v1

---
+++ pbl/footer.php
@@ -0,0 +1,20 @@
+<div id="footer">
+	<div class="wrapper" style="padding: 0;">
+		<ul>
+			<li><a href="#uvod">Úvod</a></li>
+			<li><a href="#o-nas">O nás</a></li>
+			<li><a href="#sluzby">Služby</a></li>
+			<li><a href="#reference">Reference</a></li>
+			<li><a href="#kontakt">Kontakt</a></li>
+		</ul>
+		<div class="copyright">
+			© 2016 - pblm.cz
+		</div>
+	</div>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+<script src="js/jquery.appear.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
---
+++ pbl/main.php
@@ -0,0 +1,174 @@
+<div class="page" id="uvod">
+	<div class="wrapper">
+		<div class="title">Lorem ipsum dolor sit amet, consectetur</div>
+		<p class="perex">Labore et dolore magna aliqua. Ut enim ad minim veniam,	quis nostrud exercitation</p>
+		<div class="scroll-icon"></div>
+	</div>
+	<div class="bg-mask">
+	</div>
+</div>
+<div class="page" id="o-nas">
+	<div class="wrapper">
+		<h1>O společnosti</h1>
+		<p class="subtitle">Víme co děláme a rádi Vám pomáháme, proto nás to baví!</p>
+		<p>
+		Jsme malá, dynamická a ambiciózní společnost PBL Migliori Futuro s.r.o., která díky svým pracovníkům aktivně pomáhá podnikům s jejich cíli a plány. Primárně se zaměřujeme na získávání dotací z evropských fondů a služby s tím spojené. Do současné chvíle jsme zpracovali žádosti v celkové výši přesahující 50 mil Kč s 95% úspěšností. 
+		S naší vervou se rovněž vrháme do obstarávání nemovitostí na míru pro naše klienty. 
+
+		Naším cílem je být klientům dlouhodobým a stabilním partnerem s jasnou strategií a výbornou perspektivou pro další spolupráci.
+
+		</p>
+
+	</div>
+</div>
+<div class="page" id="sluzby">
+	<div class="wrapper">
+		<h1>Námi nabízené služby</h1>
+		<p class="subtitle">Podívejte se, co umíme</p>
+		<div class="service granty animate from-left">
+			<div class="service-wrapper">
+				<div class="icon">
+				</div>
+				<div class="service-content">
+					<h2>Granty a dotace</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				</div>
+			</div>
+		</div>
+		<div class="service reality animate from-right">
+			<div class="service-wrapper">
+				<div class="icon">
+				</div>
+				<div class="service-content">
+					<h2>Realitní činnost</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				</div>
+			</div>
+		</div>
+		<div class="service atelier animate from-left">
+				<div class="service-wrapper">
+				<div class="icon">
+				</div>
+				<div class="service-content">
+					<h2>Projekční ateliér</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				</div>
+			</div>
+		</div>
+		<div class="service doprava animate from-right">
+			<div class="service-wrapper">
+				<div class="icon">
+				</div>
+				<div class="service-content">
+					<h2>Nákladní autodoprava</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="page" id="reference">
+	<div class="wrapper">
+		<h1>Reference</h1>
+		<p class="subtitle">O naší dosavadní práci vypovídají referenční zakázky, kterým se v současnosti věnujeme nebo které jsme v minulosti realizovali.</p>
+		<div class="ref">
+			<div class="ref-icon"></div>
+			<h3>Lorem ipsum dolor sit</h3>
+			<p class="sub">
+			Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			</p>
+		</div><div class="ref">
+			<div class="ref-icon"></div>
+			<h3>Lorem ipsum dolor sit</h3>
+			<p class="sub">
+			Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			</p>
+		</div><div class="ref">
+			<div class="ref-icon"></div>
+			<h3>Lorem ipsum dolor sit</h3>
+			<p class="sub">
+			Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			</p>
+		</div><div class="ref">
+			<div class="ref-icon"></div>
+			<h3>Lorem ipsum dolor sit</h3>
+			<p class="sub">
+			Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			</p>
+		</div><div class="ref">
+			<div class="ref-icon"></div>
+			<h3>Lorem ipsum dolor sit</h3>
+			<p class="sub">
+			Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			</p>
+		</div><div class="ref">
+			<div class="ref-icon"></div>
+			<h3>Lorem ipsum dolor sit</h3>
+			<p class="sub">
+			Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			</p>
+		</div>
+		<hr/>
+		<div class="ref-btns">
+			<h3 class="dark">Lorem ipsum dolor sit</h3>
+			<a href="granty-a-dotace.php#reference" class="btn ghost reference-granty">
+				<div class="icon"></div>
+				<span>Granty a Dotace</span>
+			</a>
+			<div class="btn ghost reference-reality">
+				<div class="icon"></div>
+				<span>Realitní činnost</span>
+			</div>
+			<div class="btn ghost reference-atelier">
+				<div class="icon"></div>
+				<span>Projekční ateliér</span>
+			</div>
+			</div>
+	</div>
+
+</div>
+<div class="page" id="kontakt">
+	<div class="wrapper">
+		<h1>Kontakt</h1>
+		<p class="subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+		<hr/>
+		<div class="box three">
+			<div class="icon logo-contact"></div>
+			<p>
+				<b>PBL Migliori Futuro s.r.o.</b></br>
+				V Jámě 1/699</br>
+				110 00 Praha 1 - Staré Město</br></br>
+
+				IČ: 037 13 148</br>
+				DIČ: CZ037 13 148</br>
+			</p>
+		</div><div class="box three">
+			<div class="icon phone"></div>
+			<p>
+				<b>+420 724 444 999</b>
+			</p>
+			<div class="icon mail"></div>
+			<p>
+				<b>info@pblm.cz</b>
+			</p>		
+		</div><div class="box three gf">
+			<div class="btn green big">Napište nám</div>
+			<div class="social-icons">
+				<div class="icon facebook"></div>
+				<div class="icon linkedin"></div>
+				<div class="icon twitter"></div>
+			</div>
+		</div>
+
+		</p>
+
+	</div>
+</div>
